f0f35fb993a61a0aff13c814a269b9564bb65947
Vladimir Smozhniy <Smojniy@yandex.ru>
2024-07-02 16:00:28 +0300

    Promo.py

--- Promo.py
+++ Promo.py
@@ -1,9 +1,22 @@
+# import pandas as pd
 # import random
+
 # lst = ['robot'] * 10
 # lst += ['human'] * 10
 # random.shuffle(lst)
 # data = pd.DataFrame({'whoAmI':lst})
 # data.head()
+# print(data)
 
-data = pd.DataFrame(random.sample(['robot', 'human']*10, 10) ,columns={'WhoAmI'})  
-data.head() 
+
+
+
+# import random
+# import pandas as pd
+
+# data = pd.DataFrame(random.sample(['robot', 'human']*10, 10) ,columns={'WhoAmI'})  
+# data.head() 
+# print(data)
+
+
+
